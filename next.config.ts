11bc33f5724c93dd454ade2478f96cd10c308811
Jadest13 <jadest13x03@gmail.com>
2026-02-08 23:42:00 +0900

    chore(infra): add Vanilla-Extract CSS

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 import type { NextConfig } from "next";
+import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+
+const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
   turbopack: {
@@ -11,4 +14,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withVanillaExtract(nextConfig);
