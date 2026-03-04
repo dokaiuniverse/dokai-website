6680118793b337b97e369289f9efbfc89d27a20d
Jadest13 <jadest13x03@gmail.com>
2026-03-05 03:45:47 +0900

    fix(chore): cache error

--- src/app/(header)/(footer)/page.tsx
+++ src/app/(header)/(footer)/page.tsx
@@ -1,6 +1,3 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 import MainPageClient from "./page-client";
 
 const MainPage = async () => {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "@styles/global.css";
